8edb7a3a4f6cc7ee495d9aee5bd03116173fe76f
shariq1060 <muhammadshariq@pk.folio3.com>
2022-12-16 19:35:02 +0500

    5 Hooks Added

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,8 +1,71 @@
+import React ,{useState,useReducer,useEffect,useRef} from "react";
+import Auth from "./Auth";
+import AuthContext from "./auth-context";
 
 function App() {
+
+  const Reducer=(state,action) =>{
+    switch(action.type){
+      case "INCREMENT":
+      return {count2: state.count2+1 ,show: state.show }
+      case "toggleShowText":
+      return {count2: state.count2 ,show: !state.show }
+      default:
+        return state
+    }
+}
+  const inputRef = useRef(null)
+  const [state,setState] = useReducer(Reducer, {count2:0, show:true })
+  const [value,setValue] = useState(0)
+  const [authstatus, setauthstatus] = useState(false);
+
+  const login = () => {
+    setauthstatus(true);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setValue((value) => value + 1);
+    }, 0);  
+  }, []);
+  
+  const Increment =() =>{
+    setValue(value+1)
+  }
+
+  const Decrement =() =>{
+    setValue(value-1)
+  }
+
+  const RefExample = () =>{
+    inputRef.current.value="I am here with the help of refHook :)"
+  } 
+  
   return (
     <div >
-        <h1>Hooks</h1>
+
+          <button onClick={Decrement} >Deccrement</button>
+          {value}
+          <button onClick={Increment}>Increment</button>
+          <p>-----------------------------------------------------------------------------------</p> 
+          <br/>
+          {state.count2}
+          <button onClick={ () =>{
+            setState({type:"INCREMENT"});
+            setState({type:"toggleShowText"});
+          }}>Increment</button>
+          {state.show && <h3> Use Reducer do the trick here </h3>}
+          <p>-----------------------------------------------------------------------------------</p>
+          <br/>
+          <input size={50} type="text" placeholder="Its Refhook, Give it a try........" ref={inputRef}></input>
+          <button onClick={RefExample} >Refhook</button>
+          <p>-----------------------------------------------------------------------------------</p>
+          <React.Fragment>
+            <AuthContext.Provider value={{ status: authstatus, login: login }}>
+               <Auth />
+            </AuthContext.Provider>
+          </React.Fragment>
+          <p>-----------------------------------------------------------------------------------</p>
     </div>
   );
 }
---
+++ hooks/src/Auth.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import AuthContext from "./auth-context";
+
+const Auth = () => {
+
+const auth = useContext(AuthContext);
+console.log(auth.status);
+return (
+	<div>
+	<h1>(Use Context)Are you authenticated?</h1>
+	{auth.status ?
+
+<p>Yes you are</p>
+
+:
+
+<p>Nopes</p>
+
+}
+
+	<button onClick={auth.login}>Click To Login</button>
+	</div>
+);
+};
+export default Auth;
